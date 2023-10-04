09a9b7ca315ff2eb57815233a6f236ebe8b89d64
amoreno1413 <amae2019@mymail.pomona.edu>
2023-10-04 12:00:35 -0700

    Added project

---
+++ README.md
@@ -0,0 +1,39 @@
+# Interview Questions Quiz
+This quiz is a user-friendly and customizable application built in Python with a graphical user interface using the Tkinter library. It allows you to create and take quizzes on a variety of topics. The application provides an interactive and engaging way to test your knowledge of typical interview and data structure questions.
+
+Key Features:
+
+<ul>
+    <li><strong>Quiz Creation:</strong> Easily create quizzes by entering questions and multiple-choice answers.</li>
+    <li><strong>Quiz-Taking:</strong> Take quizzes with shuffled answers, making each attempt unique.</li>
+    <li><strong>Score Tracking:</strong> Keep track of your score as you progress through the quiz.</li>
+    <li><strong>Restart Quiz:</strong> Start the quiz again from the beginning at any time.</li>
+    <li><strong>Topic Selection:</strong> Choose from a list of available quiz topics or create your own custom quizzes.</li>
+    <li><strong>Randomization:</strong> Answers are presented in random order for added challenge.</li>
+    <li><strong>Customizable:</strong> Add your questions and answers to test your knowledge in any subject.</li>
+</ul>
+This quiz is an open-source project, so you can customize and extend it to suit your needs. It's an ideal tool for anyone looking to prepare for an interview. <br>
+# TODO:
+<ul>
+  <li>
+    Make aesthetic changes to the quiz
+  </li>
+    <li>
+    Add more questions
+  </li>
+    <li>
+    Add categories to questions i.e General, Data Structures, etc
+  </li>
+  <li>
+    Make wrong answers not so obvious
+  </li>
+  <li>
+    Randomize question order 
+  </li>
+  <li>
+    Allow user to specify how many questions are shown throughout the quiz
+  </li>
+  <li>
+     Keep track of questions missed by the user   
+  </li>
+</ul>
---
+++ main.py
@@ -0,0 +1,306 @@
+import random
+import tkinter as tk
+from tkinter import ttk
+import json
+
+
+class General(tk.Toplevel):
+    def __init__(self, parent):
+
+        with open("GeneralQ.json") as f:
+            self.question = json.load(f)
+
+        self.ans = [answer[0] for answer in self.question.values()]
+
+        self.options = list(self.question.values())
+        super().__init__(parent)
+
+        self.app = parent
+        self.attributes("-fullscreen", True)
+        self.title('General Questions Quiz')
+
+        self.userAns = tk.StringVar()
+        self.userAns.set('None')
+        self.userScore = tk.IntVar()
+        self.userScore.set(0)
+        self.currQ = 0
+
+        self.nextButton = tk.Button(self, text="Next Question",
+                                    command=self.nextQ, font="calibre 30 bold", pady=20)
+
+        self.reset = tk.Button(self, text="Start Again",
+                               command=self.resetQuiz,
+                               font="calibre 17 bold", width=50, height=5)
+
+        self.f1 = ttk.Frame(self)
+        self.f1.pack(side=tk.TOP, fill=tk.X)
+
+        self.startQuiz()
+
+    def startQuiz(self):
+        self.nextButton.pack()
+        self.nextQ()
+
+    def nextQ(self):
+        if self.currQ < len(self.question):
+            self.checkAns()
+            self.userAns.set("None")
+            cQuestion = list(self.question.keys())[self.currQ]
+            self.clearFrame()
+
+            tk.Label(self.f1, text=f"{self.currQ + 1}: {cQuestion}", padx=15, pady=100, font="calibre 50 bold").pack()
+            opt = list(self.question[cQuestion])
+            random.shuffle(opt)
+
+            for option in opt:
+                tk.Radiobutton(self.f1, text=option, variable=self.userAns, value=option, padx=28,
+                               font="calibre 30 normal").pack()
+            self.currQ += 1
+
+            tk.Label(self.f1, text=f"Your score: {self.userScore.get()} / {len(self.question)}", padx=15,
+                     font="calibre 20 normal").pack(
+                anchor=tk.SE)
+        else:
+            self.nextButton.forget()
+            self.checkAns()
+            self.clearFrame()
+            output = "Reset?"
+            tk.Label(self.f1, text=output, font="calibre 25 bold").pack()
+            tk.Label(self.f1, text="Thanks for Participating", font="calibre 18 bold").pack()
+
+            self.reset.pack()
+
+    def resetQuiz(self):
+        self.currQ = 0
+        self.userScore.set(0)
+        self.reset.forget()
+        self.startQuiz()
+
+    def checkAns(self):
+        temp = self.userAns.get()
+        if temp != "None" and temp == self.ans[self.currQ - 1]:
+            self.userScore.set(self.userScore.get() + 1)
+
+    def clearFrame(self):
+        for widget in self.f1.winfo_children():
+            widget.destroy()
+
+
+class Data(tk.Toplevel):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        with open("DataStrucQ.json") as f:
+            self.question = json.load(f)
+
+        self.ans = [answer[0] for answer in self.question.values()]
+
+        self.options = list(self.question.values())
+
+        self.app = parent
+        self.attributes("-fullscreen", True)
+        self.title('Data Structures Quiz')
+
+        self.userAns = tk.StringVar()
+        self.userAns.set('None')
+        self.userScore = tk.IntVar()
+        self.userScore.set(0)
+        self.currQ = 0
+
+        self.nextButton = tk.Button(self, text="Next Question",
+                                    command=self.nextQ, font="calibre 30 bold", pady=20)
+
+        self.reset = tk.Button(self, text="Start Again",
+                               command=self.resetQuiz,
+                               font="calibre 17 bold", width=50, height=5)
+
+        self.f1 = ttk.Frame(self)
+        self.f1.pack(side=tk.TOP, fill=tk.X)
+
+        self.startQuiz()
+
+    def startQuiz(self):
+        self.nextButton.pack()
+        self.nextQ()
+
+    def nextQ(self):
+        if self.currQ < len(self.question):
+            self.checkAns()
+            self.userAns.set("None")
+            cQuestion = list(self.question.keys())[self.currQ]
+            self.clearFrame()
+
+            tk.Label(self.f1, text=f"{self.currQ + 1}: {cQuestion}", padx=15, pady=100, font="calibre 50 bold").pack()
+            opt = list(self.question[cQuestion])
+            random.shuffle(opt)
+
+            for option in opt:
+                tk.Radiobutton(self.f1, text=option, variable=self.userAns, value=option, padx=28,
+                               font="calibre 30 normal").pack()
+            self.currQ += 1
+
+            tk.Label(self.f1, text=f"Your score: {self.userScore.get()} / {len(self.question)}", padx=15,
+                     font="calibre 20 normal").pack(
+                anchor=tk.SE)
+        else:
+            self.nextButton.forget()
+            self.checkAns()
+            self.clearFrame()
+            output = "Reset?"
+            tk.Label(self.f1, text=output, font="calibre 25 bold").pack()
+            tk.Label(self.f1, text="Thanks for Participating", font="calibre 18 bold").pack()
+
+            self.reset.pack()
+
+    def resetQuiz(self):
+        self.currQ = 0
+        self.userScore.set(0)
+        self.reset.forget()
+        self.startQuiz()
+
+    def checkAns(self):
+        temp = self.userAns.get()
+        if temp != "None" and temp == self.ans[self.currQ - 1]:
+            self.userScore.set(self.userScore.get() + 1)
+
+    def clearFrame(self):
+        for widget in self.f1.winfo_children():
+            widget.destroy()
+
+
+class Algorithms(tk.Toplevel):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        with open("Algorithms.json") as f:
+            self.question = json.load(f)
+
+        self.ans = [answer[0] for answer in self.question.values()]
+
+        self.options = list(self.question.values())
+
+        self.app = parent
+        self.attributes("-fullscreen", True)
+        self.title('Algorithms Quiz')
+
+        self.userAns = tk.StringVar()
+        self.userAns.set('None')
+        self.userScore = tk.IntVar()
+        self.userScore.set(0)
+        self.currQ = 0
+
+        self.nextButton = tk.Button(self, text="Next Question",
+                                    command=self.nextQ, font="calibre 30 bold", pady=20)
+
+        self.reset = tk.Button(self, text="Start Again",
+                               command=self.resetQuiz,
+                               font="calibre 17 bold", width=50, height=5)
+
+        self.f1 = ttk.Frame(self)
+        self.f1.pack(side=tk.TOP, fill=tk.X)
+
+        self.backFrame = ttk.Frame(self)
+        self.backFrame.place(relx=0.5, rely=.5, anchor='sw')
+        ttk.Button(self.backFrame, text="Back",
+                   command=self.back).pack(side='left')
+
+        self.startQuiz()
+
+    def startQuiz(self):
+        self.nextButton.pack()
+        self.nextQ()
+
+    def nextQ(self):
+        if self.currQ < len(self.question):
+            self.checkAns()
+            self.userAns.set("None")
+            cQuestion = list(self.question.keys())[self.currQ]
+            self.clearFrame()
+
+            tk.Label(self.f1, text=f"{self.currQ + 1}: {cQuestion}", padx=15, pady=100, font="calibre 50 bold").pack()
+            opt = list(self.question[cQuestion])
+            random.shuffle(opt)
+
+            for option in opt:
+                tk.Radiobutton(self.f1, text=option, variable=self.userAns, value=option, padx=28,
+                               font="calibre 30 normal").pack()
+            self.currQ += 1
+
+            tk.Label(self.f1, text=f"Your score: {self.userScore.get()} / {len(self.question)}", padx=15,
+                     font="calibre 20 normal").pack(
+                anchor=tk.SE)
+        else:
+            self.nextButton.forget()
+            self.checkAns()
+            self.clearFrame()
+            output = "Reset?"
+            tk.Label(self.f1, text=output, font="calibre 25 bold").pack()
+            tk.Label(self.f1, text="Thanks for Participating", font="calibre 18 bold").pack()
+
+            self.reset.pack()
+
+    def resetQuiz(self):
+        self.currQ = 0
+        self.userScore.set(0)
+        self.reset.forget()
+        self.startQuiz()
+
+    def checkAns(self):
+        temp = self.userAns.get()
+        if temp != "None" and temp == self.ans[self.currQ - 1]:
+            self.userScore.set(self.userScore.get() + 1)
+
+    def back(self):
+        self.app.deiconify()
+        self.destroy()
+
+    def clearFrame(self):
+        for widget in self.f1.winfo_children():
+            widget.destroy()
+
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.attributes("-fullscreen", True)
+        self.title("Quiz App")
+
+        genStart = tk.Button(self, text="General Quiz",
+                             command=self.openGen,
+                             font="calibre 17 bold", width=50, height=5)
+
+        genStart.pack(side='left', expand=True)
+
+        dataStart = tk.Button(self, text="Data Structures Quiz",
+                              command=self.openDS,
+                              font="calibre 17 bold", width=50, height=5)
+
+        dataStart.pack(side='left', expand=True)
+
+        algoStart = tk.Button(self, text="Algorithm Quiz",
+                              command=self.openAl,
+                              font="calibre 17 bold", width=50, height=5)
+
+        algoStart.pack(side="bottom", expand=True)
+
+        ttk.Label(self, text='Choose a quiz', font="calibre 40 normal").place(relx=.5, rely=0.2, anchor='center')
+
+    def openGen(self):
+        newWindow = General(self)
+        newWindow.grab_set()
+        self.withdraw()
+
+    def openDS(self):
+        newWindow = Data(self)
+        newWindow.grab_set()
+        self.withdraw()
+
+    def openAl(self):
+        newWindow = Algorithms(self)
+        newWindow.grab_set()
+        self.withdraw()
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
